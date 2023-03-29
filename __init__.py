7be8f7a9ad148fc294ac630d4a03759214edfd12
Beatrice Apollo <beatrice.apollo.00@gmail.com>
2023-03-29 17:30:44 +0200

    moved the onto loading inside the skill class

--- __init__.py
+++ __init__.py
@@ -7,23 +7,21 @@ from nltk import word_tokenize
 from owlready2 import *
 import owlready2
 
-
-#carico l'ontologia all'interno dello script:
-onto = get_ontology("file://HBAwithEnergy.owl").load()
-
-#definisco la lista di tutte le object property dell'ontologia e la stampo:
-object_properties_list = list(onto.object_properties())
-print(object_properties_list)
-
-#definisco la lista di tuple contenente le associazioni proprietà-range e la stampo:
-associations = []
-for o in object_properties_list:
-    associations.append((o,o.range))
-    print(associations)
-
 class Postagging(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        #carico l'ontologia all'interno dello script:
+        onto = get_ontology("file://HBAwithEnergy.owl").load()
+
+        #definisco la lista di tutte le object property dell'ontologia e la stampo:
+        object_properties_list = list(onto.object_properties())
+        print(object_properties_list)
+
+        #definisco la lista di tuple contenente le associazioni proprietà-range e la stampo:
+        associations = []
+        for o in object_properties_list:
+            associations.append((o,o.range))
+            print(associations)
 
     @intent_handler('postagging.intent')
     def handle_postagging(self, message):
