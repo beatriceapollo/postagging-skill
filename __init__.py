9a0e5667d29040af4ed14a8f0f1d6f58170923d6
Beatrice Apollo <beatrice.apollo.00@gmail.com>
2023-03-30 17:00:54 +0200

    test

--- __init__.py
+++ __init__.py
@@ -7,9 +7,6 @@ from nltk import word_tokenize
 from owlready2 import *
 import owlready2
 
-#carico l'ontologia all'interno dello script:
-onto = get_ontology("file://./HBAwithEnergy.owl").load()
-
 class Postagging(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
@@ -30,6 +27,9 @@ class Postagging(MycroftSkill):
 
     @intent_handler(IntentBuilder('IdentityIntent').require('identity'))
     def handle_identity_intent(self, message):
+
+        #carico l'ontologia all'interno dello script:
+        onto = get_ontology("file://./HBAwithEnergy.owl").load()
 
         #definisco la lista di tutte le object property dell'ontologia e la stampo:
         #object_properties_list = list(onto.object_properties())
