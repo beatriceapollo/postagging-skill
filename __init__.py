ff58c38cb5e5e79af64f59b6d7c7aaa0f3813e3c
Beatrice Apollo <beatrice.apollo.00@gmail.com>
2023-03-30 16:54:31 +0200

    fixed error

--- __init__.py
+++ __init__.py
@@ -7,14 +7,13 @@ from nltk import word_tokenize
 from owlready2 import *
 import owlready2
 
+#carico l'ontologia all'interno dello script:
+onto = get_ontology("file://./HBAwithEnergy.owl").load()
 
 class Postagging(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-        #carico l'ontologia all'interno dello script:
-        onto = get_ontology("file://./HBAwithEnergy.owl").load()
-
-
+        
     #definisco la lista di tutte le object property dell'ontologia e la stampo:
     #object_properties_list = list(onto.object_properties())
     #print(object_properties_list)
@@ -31,8 +30,6 @@ class Postagging(MycroftSkill):
 
     @intent_handler(IntentBuilder('IdentityIntent').require('identity'))
     def handle_identity_intent(self, message):
-            #carico l'ontologia all'interno dello script:
-        onto = get_ontology("HBAwithEnergy.owl").load()
 
         #definisco la lista di tutte le object property dell'ontologia e la stampo:
         #object_properties_list = list(onto.object_properties())
