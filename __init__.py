0e9ca7aedb0a4c8973af037a1e07bf83607a1062
Beatrice Apollo <beatrice.apollo.00@gmail.com>
2023-03-30 18:09:15 +0200

    PROVA

--- __init__.py
+++ __init__.py
@@ -7,19 +7,24 @@ from nltk import word_tokenize
 from owlready2 import *
 import owlready2
 
+
+#carico l'ontologia all'interno dello script:
+onto = get_ontology("/home/beatrice/skill1/postagging-skill/HBAwithEnergy.owl").load()
+
+#definisco la lista di tutte le object property dell'ontologia:
+object_properties_list = list(onto.object_properties())
+#print(object_properties_list)
+
+#definisco la lista di tuple contenente le associazioni proprietà-range:
+associations = []
+for o in object_properties_list:
+    associations.append((o,o.range))
+    #print(associations)
+
+
 class Postagging(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-        
-    #definisco la lista di tutte le object property dell'ontologia e la stampo:
-    #object_properties_list = list(onto.object_properties())
-    #print(object_properties_list)
-
-    #definisco la lista di tuple contenente le associazioni proprietà-range e la stampo:
-    #associations = []
-    #for o in object_properties_list:
-        #associations.append((o,o.range))
-        #print(associations)
 
     @intent_handler('postagging.intent')
     def handle_postagging(self, message):
@@ -28,18 +33,6 @@ class Postagging(MycroftSkill):
     @intent_handler(IntentBuilder('IdentityIntent').require('identity'))
     def handle_identity_intent(self, message):
 
-        #carico l'ontologia all'interno dello script:
-        onto = get_ontology("/home/beatrice/skill1/postagging-skill/HBAwithEnergy.owl").load()
-
-        #definisco la lista di tutte le object property dell'ontologia e la stampo:
-        object_properties_list = list(onto.object_properties())
-        print(object_properties_list)
-
-        #definisco la lista di tuple contenente le associazioni proprietà-range e la stampo:
-        associations = []
-        for o in object_properties_list:
-            associations.append((o,o.range))
-            print(associations)
         #recupero la frase pronunciata dall'utente:
         text = message.data.get('utterance')
         #effettuo il pos tagging della frase:
@@ -51,9 +44,9 @@ class Postagging(MycroftSkill):
         {<VB.*><.*>*<NN.*>}
         ''')
 
-        #effettuo la prima operazione di chunking e stampo l'albero risultante:
+        #effettuo la prima operazione di chunking:
         output = chunker.parse(tagged_text) 
-        print(output) 
+        #print(output) 
 
         #estraggo e stampo il sottoalbero che rappresenta il pattern taggato con P1:
         for subtree in output.subtrees(filter=lambda t: t.label() == 'P1'):
@@ -65,26 +58,26 @@ class Postagging(MycroftSkill):
         {<NN.*>}
         ''')
                         
-        #effettuo la seconda operazione di chunking sul sottoalbero di prima e stampo l'albero risultante:
+        #effettuo la seconda operazione di chunking sul sottoalbero di prima:
         output2 = chunker2.parse(subtree)
-        print(output2) 
+        #print(output2) 
 
         #estraggo e stampo il secondo sottoalbero che rappresenta il pattern taggato con P2:
         for subtree2 in output2.subtrees(filter=lambda t: t.label() == 'P2'):
             print(subtree2)
 
-        #trasformo il sottalbero estratto in una stringa e lo stampo con la prima lettera maiuscola:
+        #trasformo il sottalbero estratto in una stringa con la prima lettera maiuscola:
         Concept_string = (' '.join([x[0] for x in subtree2])).capitalize()
-        print(Concept_string)
+        #print(Concept_string)
 
         #effettuo la ricerca del concetto estratto all'interno dell'ontologia e stampo il risultato:
         Concept = onto.search(iri = "*"+Concept_string)
         ConceptMatch = Concept[0]
         print(ConceptMatch)
 
-        #definisco la lista degli "antenati" del concetto estratto e la stampo:
+        #definisco la lista degli "antenati" del concetto estratto:
         Ancestors = list(Concept[0].ancestors())
-        print(Ancestors)
+        #print(Ancestors)
 
         #cerco tra gli antenati la classe che è un range di una proprietà presente nell'ontologia;
         #salvo all'interno di variabili la classe individuata come range e la proprietà associata:
@@ -107,7 +100,6 @@ class Postagging(MycroftSkill):
             class NewClass(Thing):
                 equivalent_to = class_expression
 
-        self.speak(class_expression)
 
     @intent_handler(IntentBuilder('StopIntent').require('stop'))
     def handle_stop_intent(self, message):
